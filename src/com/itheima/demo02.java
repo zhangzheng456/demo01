2b9e11c06018270f3f6fb9490bd5c942bda79e36
zz <158314655@qq.com>
2019-02-24 20:42:06 +0800

    第一次提交

---
+++ src/com/itheima/demo02.java
@@ -0,0 +1,41 @@
+package com.itheima;
+
+public class demo02 {
+    public static void main(String[] args) {
+        me();
+        gequ ();
+    }
+
+    public static void me(){
+        for (int i = 0; i < 5; i++) {
+            System.out.print("123");
+            for (int i1 = 1; i1 <=1; i1++) {
+                System.out.print("木头人");
+                for (int i2 = 1; i2 <=1; i2++) {
+                    System.out.print("好听");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void gequ() {
+        for (int i = 0; i < 10; i++) {
+            System.out.print ("哈登");
+            for (int i1 = 0; i1 < 1; i1++) {
+                System.out.print ("是个");
+                for (int j = 0; j < 1; j++) {
+                    System.out.print ("大胡子");
+                }
+
+
+            }
+            System.out.println ();
+
+        }
+
+    }
+
+}
